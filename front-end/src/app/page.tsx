1b12628499d5d703bf06e96210cf5ad9a8675f97
Mohak157 <girimohak73@gmail.com>
2026-08-17 18:14:49 +0545

    chore: fixed minor inconveniences

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,40 +1,14 @@
-
-import HandSpin from "@/components/HandSpin";
 import Starfield from "@/components/Starfield";
 
 
-
-
-
-
-
-
-import InfiniteCarousel from "@/components/InfiniteCarousel";
-
-  
-
 const page = () => {
   return (
-<<<<<<< HEAD
    <>
 
    <Starfield/>
-   <HandSpin/>
+  
    
    </>
-=======
-    <>
-
-
-
-
-
-
-
-    
-     <InfiniteCarousel/>
-    </>
->>>>>>> 37d342462e06b1b366167ca999a49c3d35b214ed
   )
 }
 
